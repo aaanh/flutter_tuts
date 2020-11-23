e35160ed4e1b9fe976b43c0553952c2dba3faeee
HA <hoanganh.theodore@icloud.com>
2020-11-23 01:35:32 -0500

    continue tut

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,18 @@ import 'package:flutter/material.dart';
 // using arrow function for functions with ONLY 1 expression
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0; // inferred as int, called property
+class MyApp extends StatefulWidget {
   
+  @override // needs to be overwritten
+  State<StatefulWidget> createState() {
+    
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0; // inferred as int, called property
+
   void answerQuestion() {
     questionIndex = questionIndex + 1;
     if (questionIndex > 1) {
